cbcd81dd23387fb6bc9e20f6f9e5a61eeb6e4c31
Nadaraj Prabhu <42749054+nadaraj15@users.noreply.github.com>
2019-02-08 23:27:05 +0530

    Upload script file
    Upload script file

---
+++ VM_DiskEncrypt.ps1
@@ -0,0 +1,47 @@
+﻿<#
+**********************Note*************************
+Replace the parametes before executing
+
+by Nadaraj Prabhu 
+
+$SubName        - Subscription Name
+$KeyVaultrgName - Key Vault Resource Group Name
+$VMrgName       - Resource Group Name
+$VMs            - VM Name
+
+you can add multiple VM in same resource group:
+
+$VMs += "Dev1VM"
+$VMs += "Dev2VM"
+$VMs += "Dev3VM"
+
+#>
+
+$SubName ="your Subscription name"
+$VMs = @()
+$VMs += "DevVM"
+$VMrgName = ‘Dev-RG’;
+
+
+<#
+$KeyVaultName - Azure Key vault Name
+$KeyVaultKey  - Name of the key in the Key vault 
+#>
+
+$KeyVaultName = 'vmcypherkeys';
+$keyEncryptionKeyName = 'Bitlocker-Key';
+$rgName = 'Dev-RG';
+
+Login-AzureRmAccount
+Select-AzureRMSubscription -SubscriptionName $SubName
+
+$KeyVault = Get-AzureRmKeyVault -VaultName $KeyVaultName -ResourceGroupName $rgname;
+$diskEncryptionKeyVaultUrl = $KeyVault.VaultUri;
+$KeyVaultResourceId = $KeyVault.ResourceId;
+$keyEncryptionKeyUrl = (Get-AzureKeyVaultKey -VaultName $KeyVaultName -Name $keyEncryptionKeyName).Key.kid;
+
+
+#Script to enable Azure Disk Encryption
+foreach ($VMName in $VMs) {
+Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $VMrgName -VMName $vmName -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl -DiskEncryptionKeyVaultId $KeyVaultResourceId -KeyEncryptionKeyUrl $keyEncryptionKeyUrl -KeyEncryptionKeyVaultId $KeyVaultResourceId -VolumeType All -SequenceVersion $sequenceVersion -Force -debug;
+};
